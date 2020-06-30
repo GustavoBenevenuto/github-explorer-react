4fd7072ae9adbee09bf97ed400bfec2d37137c92
Gustavo Benevenuto <gustavo.benesilva@gmail.com>
2020-06-30 14:48:11 -0300

    Tratamento de erros

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,49 +1,77 @@
-import React from 'react';
-import {Title, Form, Repositories} from './style';
+import React, { useState, FormEvent } from 'react';
+import { Title, Form, Repositories, Error } from './style';
 import LogoImage from '../../assets/logo.svg';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+interface Repository {
+    full_name: string;
+    description: string;
+    owner: {
+        login: string;
+        avatar_url: string;
+    };
+}
 
 const Dashboard: React.FC = () => {
+
+    const [reposiotires, setRepositories] = useState<Repository[]>([]);
+    const [newRepo, setNewRepo] = useState('');
+    const [inputError, setInputError] = useState('');
+
+    async function handleAddRepository(event: FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+
+        if (!newRepo) {
+            setInputError('Digite o autor/nome do repositório');
+            return;
+        }
+
+        try {
+            const response = await api.get<Repository>(`repos/${newRepo}`);
+
+            const repository = response.data;
+
+            setRepositories([...reposiotires, repository]);
+            setNewRepo('');
+            setInputError('');
+        } catch (e) {
+            setInputError('Erro na busca por esse repositório');
+        }
+    }
+
     return (
         <>
-            <img src={LogoImage} alt="GitHub Explorer"/>
+            <img src={LogoImage} alt="GitHub Explorer" />
             <Title>Explore repositórios no GitHub</Title>
 
-            <Form>
-                <input type="text" placeholder="Digite o nome do repositório"/>
+            <Form onSubmit={handleAddRepository} hasError={Boolean(inputError)}>
+                <input type="text"
+                    placeholder="Digite o nome do repositório"
+                    value={newRepo} onChange={event => setNewRepo(event.target.value)} />
                 <button type="submit">Pesquisar</button>
             </Form>
 
+            {inputError &&
+                <Error>
+                    {inputError}
+                </Error>
+            }
+
             <Repositories>
-                <a href="#">
-                    <img src='https://avatars1.githubusercontent.com/u/40447101?s=400&u=0e966219760b77dbb32e1f728c4f2e3dc1c41e99&v=4' alt="Gustavo"/>
+                {reposiotires.map((item, index) => {
+                    return (
+                        <a href="#" key={index}>
+                            <img src={item.owner.avatar_url} alt={item.owner.login} />
 
-                    <div>
-                        <strong>GustavoBenevenuto/Clone-Meu-Universo</strong>
-                        <p>Clone da aplicação mobile "Meu Universo". Clone da aplicação mobile "Meu Universo". </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="#">
-                    <img src='https://avatars1.githubusercontent.com/u/40447101?s=400&u=0e966219760b77dbb32e1f728c4f2e3dc1c41e99&v=4' alt="Gustavo"/>
-
-                    <div>
-                        <strong>GustavoBenevenuto/Clone-Meu-Universo</strong>
-                        <p>Clone da aplicação mobile "Meu Universo". Clone da aplicação mobile "Meu Universo". </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
-
-                <a href="#">
-                    <img src='https://avatars1.githubusercontent.com/u/40447101?s=400&u=0e966219760b77dbb32e1f728c4f2e3dc1c41e99&v=4' alt="Gustavo"/>
-
-                    <div>
-                        <strong>GustavoBenevenuto/Clone-Meu-Universo</strong>
-                        <p>Clone da aplicação mobile "Meu Universo". Clone da aplicação mobile "Meu Universo". </p>
-                    </div>
-                    <FiChevronRight size={20} />
-                </a>
+                            <div>
+                                <strong>{item.full_name}</strong>
+                                <p>{item.description}</p>
+                            </div>
+                            <FiChevronRight size={20} />
+                        </a>
+                    )
+                })}
             </Repositories>
         </>
     )
